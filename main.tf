8cb5832ba5de70f760dc643dab3259b0169221f5
Uma Krishna Marineni <129099526+umakrishna-2002@users.noreply.github.com>
2025-06-08 19:21:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "k_s1" {
   count = 3
   ami                    = "ami-0731becbf832f281e"
   instance_type          = "t2.micro"
-  key_name               = "k8s"
+  key_name               = "add your key-pair"
 vpc_security_group_ids = [data.aws_security_group.project_sg.id]
 
  user_data = count.index == 0 ? file("${path.module}/user_data.sh") : null
